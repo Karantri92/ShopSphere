2578a78182f60c7a6a4cd0ebb2d12766491cb3b4
Your GitHub Username <your_email@example.com>
2026-09-08 18:05:59 +0530

    Use environment variable for API URL

--- client/src/Pages/Homes.jsx
+++ client/src/Pages/Homes.jsx
@@ -15,7 +15,7 @@ function Home() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/products")
+   fetch(`${import.meta.env.VITE_API_URL}/api/products`)
       .then((res) => res.json())
       .then((data) => {
         console.log("HOME PRODUCTS:", data);
